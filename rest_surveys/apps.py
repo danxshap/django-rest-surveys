3411d4edd450ceaf41c38e225035b578ae4ec70a
Edgar M. Salazar <edgarmsalazar@gmail.com>
2022-01-18 10:36:19 -0500

    adding auto field requirement

--- rest_surveys/apps.py
+++ rest_surveys/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 
 class RestSurveysConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'rest_surveys'
     verbose_name = 'REST Surveys'
