3dbb53c6f8de418664347e95c6c416d5edc24ce5
Edgar M. Salazar <edgarmsalazar@gmail.com>
2022-01-18 10:57:18 -0500

    use basic auto field

--- rest_surveys/apps.py
+++ rest_surveys/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 
 class RestSurveysConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
     name = 'rest_surveys'
     verbose_name = 'REST Surveys'
